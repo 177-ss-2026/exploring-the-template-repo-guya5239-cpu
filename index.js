c92829854622733288633e8dc60c52f1659341f0
Babatunde Owoseni <babatunde.owoseni@student.swic.edu>
2026-02-19 13:15:09 -0600

    Add personal Information variables

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello via Bun (plain JS)!");
+const firstName = "Babatunde";
+const lastName = "Owoseni";
+const age = 18;
+const isStudent = true;
+
+const introduction = `Hello! My name is ${firstName} ${lastName}. I am ${age} years old.`;
+const studentStatus = `Am I a student? ${isStudent}`;
+
+console.log(introduction);
+console.log(studentStatus);
